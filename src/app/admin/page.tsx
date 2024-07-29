8be21a9755b1a579a2ea4b41054164017124642c
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-28 23:46:43 -0500

    shadcn card add

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,7 @@
 export default function AdminDashboard() {
     return (
-        <h1>Dashboard</h1>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+            
+        </div>
     )
 }

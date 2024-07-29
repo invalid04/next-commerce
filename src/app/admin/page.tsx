042a0cacedf335bf26456ea41348c7d81eac891d
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-28 23:55:58 -0500

    db file

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,9 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
+function getSalesData() {
+    
+}
+
 export default function AdminDashboard() {
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>

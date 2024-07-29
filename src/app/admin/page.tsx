71d621223cbdd0d7ebcfd4b195effc1b50705d7c
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-28 23:51:39 -0500

    DashboardCard component

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -18,3 +18,28 @@ export default function AdminDashboard() {
         </div>
     )
 }
+
+type DashboardCardProps = {
+    title: string
+    subtitle: string 
+    body: string
+}
+
+function DashboardCard({title, subtitle, body}: DashboardCardProps) {
+    return (
+            <Card>
+                <CardHeader>
+                    <CardTitle>
+                        {title}
+                    </CardTitle>
+                    <CardDescription>
+                        {subtitle}
+                    </CardDescription>
+                </CardHeader>
+                
+                <CardContent>
+                    <p>{body}</p>
+                </CardContent>
+            </Card>
+    )
+}

e6ba3e490f6a32ee9c2f0d6046d79d82704d73a2
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-31 01:47:04 -0500

    fileSchema

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -1,5 +1,16 @@
 'use server'
 
+import { z } from "zod"
+
+const fileSchema = z.instanceof(File)
+
+z.object({
+    name: z.string().min(1),
+    description: z.string().min(1),
+    priceInCents: z.coerce.number().int().min(1),
+     
+})
+
 export async function addProduct(formData: FormData) {
-    
+
 }

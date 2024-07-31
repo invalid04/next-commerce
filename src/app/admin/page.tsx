46c5009b1f9ee3252b471f999085db0bb1dc6699
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-31 01:01:05 -0500

    getUserData function

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -14,7 +14,21 @@ async function getSalesData() {
     }
 }
 
+async function getUserData() {
+    const [userCount, orderData] = await Promise.all([
+        db.user.count(),
+        db.order.aggregate({
+            _sum: { pricePaidInCents: true },
+        })
+    ])
 
+    return {
+        userCount,
+        averageValuePerUser: userCount === 0 
+            ? 0 
+            : (orderData._sum.pricePaidInCents || 0) / userCount / 100,
+    }
+}
 
 export default async function AdminDashboard() {
     const salesData = await getSalesData()

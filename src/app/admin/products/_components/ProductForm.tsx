ba18fb63251ceab89cec7e314828658b595469ea
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-31 01:37:15 -0500

    name and price input for product form

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -1,7 +1,30 @@
 'use client'
 
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useState } from "react"
+
 export default function ProductForm() {
+    const [priceInCents, setPriceInCents] = useState<number>()
+
     return (
-        
+        <form className='space-y-8'>
+            <div className='space-y-2'>
+                <Label htmlFor='name'>Name</Label>
+                <Input type='text' id='name' name='name' required />
+            </div>
+
+            <div className='space-y-2'>
+                <Label htmlFor='priceInCents'>Price in Cents</Label>
+                <Input 
+                    type='number' 
+                    id='priceInCents' 
+                    name='priceInCents' 
+                    required 
+                    value={priceInCents}
+                    onChange={e => setPriceInCents(Number(e.target.value) || undefined)}
+                />
+            </div>
+        </form>
     )
 }

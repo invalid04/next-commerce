6d32b8fe1555912bf0e562148ced86e949e0858c
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-07-31 02:20:41 -0500

    mapping through table in admin

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,7 +1,10 @@
 import { Button } from "@/components/ui/button";
 import { PageHeader } from "../_components/PageHeader";
 import Link from "next/link";
-import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import db from "@/db/db";
+import { CheckCircle2, MoreVertical, XCircle } from "lucide-react";
+import { formatCurrency, formatNumber } from "@/lib/formatters";
 
 export default function AdminProductsPage() {
     return (
@@ -19,7 +22,20 @@ export default function AdminProductsPage() {
     )
 }
 
-function ProductsTable() {
+async function ProductsTable() {
+    const products = await db.product.findMany({
+        select: {
+            id: true,
+            name: true,
+            priceInCents: true,
+            isAvailableForPurchase: true,
+            _count: { select: { orders: true }}
+        },
+        orderBy: { name: 'asc'}
+    })
+
+    if (products.length === 0) return <p>No products found</p>
+
     return (
         <Table>
             <TableHeader>
@@ -37,7 +53,29 @@ function ProductsTable() {
             </TableHeader>
 
             <TableBody>
-
+                {products.map(product => (
+                    <TableRow key={product.id}>
+                        <TableCell>
+                            {product.isAvailableForPurchase ? (
+                                <> 
+                                    <span className='sr-only'>Available</span>
+                                    <CheckCircle2 /> 
+                                </>
+                            ) : (
+                                <> 
+                                    <span className='sr-only'>Unavailable</span>
+                                    <XCircle /> 
+                                </>
+                            )}
+                        </TableCell>
+                        <TableCell>{product.name}</TableCell>
+                        <TableCell>{formatCurrency(product.priceInCents / 100)}</TableCell>
+                        <TableCell>{formatNumber(product._count.orders)}</TableCell>
+                        <TableCell>
+                            <MoreVertical />
+                        </TableCell>
+                    </TableRow>
+                ))}
             </TableBody>
         </Table>
     )

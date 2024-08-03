30daebc92891884565afe763fb01017dee1a38af
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-03 18:20:57 -0500

    getMostPopularProducts function

--- src/app/(customerFacing)/page.tsx
+++ src/app/(customerFacing)/page.tsx
@@ -1,5 +1,13 @@
 import db from "@/db/db"
 
+function getMostPopularProducts() {
+    return db.product.findMany({
+        where: { isAvailableForPurchase: true },
+        orderBy: { orders: { _count: 'desc' } },
+        take: 6
+    })
+}
+
 function getNewestProducts() {
     return db.product.findMany({
         where: { isAvailableForPurchase: true },

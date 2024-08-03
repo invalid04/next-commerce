eb90fc05adfcc4172c5d092489d1dae0fbe1ff5e
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-03 04:23:33 -0500

    editProduct action

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -63,28 +63,39 @@ const editSchema = addSchema.extend({
 })
 
 export async function updateProduct(id: string, prevState: unknown, formData: FormData) {
-    const result = addSchema.safeParse(Object.fromEntries(formData.entries()))
+    const result = editSchema.safeParse(Object.fromEntries(formData.entries()))
     if (result.success === false) {
         return result.error.formErrors.fieldErrors
     }
 
     const data = result.data
+    const product = await db.product.findUnique({ where: { id } })
 
-    await fs.mkdir('products', { recursive: true })
-    const filePath = `products/${crypto.randomUUID()}-${data.file.name}`
-    await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()))
+    if (product == null) return notFound()
 
-    await fs.mkdir('public/products', { recursive: true })
-    const imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`
-    await fs.writeFile(`public${imagePath}`, Buffer.from(await data.image.arrayBuffer()))
+    let filePath = product.filePath
+    if (data.file != null && data.file.size > 0) {
+        await fs.unlink(product.filePath)
+        filePath = `products/${crypto.randomUUID()}-${data.file.name}`
+        await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()))
+    }
 
-    await db.product.create({ data: {
-        isAvailableForPurchase: false,
-        name: data.name,
-        description: data.description,
-        priceInCents: data.priceInCents,
-        filePath,
-        imagePath
+    let imagePath = product.imagePath
+    if (data.image != null && data.image.size > 0) {
+        await fs.unlink(`public${product.imagePath}`)
+        imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`
+        await fs.writeFile(`public${imagePath}`, Buffer.from(await data.image.arrayBuffer()))
+    }
+
+    await db.product.update({ 
+        where: { id },
+        data: {
+            isAvailableForPurchase: false,
+            name: data.name,
+            description: data.description,
+            priceInCents: data.priceInCents,
+            filePath,
+            imagePath
     }})
 
     redirect('/admin/products')

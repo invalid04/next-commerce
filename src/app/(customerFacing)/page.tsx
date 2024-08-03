3e11a79173a0da78e1d5985849297fcd80e91d42
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-03 18:19:32 -0500

    getNewestProducts function

--- src/app/(customerFacing)/page.tsx
+++ src/app/(customerFacing)/page.tsx
@@ -1,3 +1,13 @@
+import db from "@/db/db"
+
+function getNewestProducts() {
+    return db.product.findMany({
+        where: { isAvailableForPurchase: true },
+        orderBy: { orders: { _count: 'desc' } },
+        take: 6
+    })
+}
+
 export default function HomePage() {
     return (
         <h1>no</h1>

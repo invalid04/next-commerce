55f079571ba236e7a2912c2fbcf6173ff9a36e57
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-03 03:34:48 -0500

    outline for active toggle component

---
+++ src/app/admin/products/_components/ProductActions.tsx
@@ -0,0 +1,19 @@
+import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
+
+export function ActiveToggleDropdownItem({
+    id,
+    isAvailableForPurchase
+}: {
+    id: string 
+    isAvailableForPurchase: boolean
+}) {
+    return (
+        <DropdownMenuItem>
+
+        </DropdownMenuItem>
+    )
+}
+
+export function DeleteToggleDropdownItem() {
+
+}

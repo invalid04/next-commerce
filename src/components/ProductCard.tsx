8f40a79510796fcd8b35e3eba397df48dc701108
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-04 05:32:51 -0500

    image section

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,10 +2,18 @@ import { formatCurrency } from "@/lib/formatters";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import Image from "next/image";
 
-export function ProductCard({ id, name, priceInCents, description }) {
+export function ProductCard({ id, name, priceInCents, description, imagePath }) {
     return (
         <Card className='flex overflow-hidden flex-col'>
+            <div className='relative w-full h-auto aspect-video'>
+                <Image 
+                    src={imagePath}
+                    fill 
+                    alt={name}
+                />
+            </div>
             <CardHeader>
                 <CardTitle>
                     {name}

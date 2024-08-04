b7d42bca991225b7fd84ebc4784e9a34309f0ed9
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-04 05:58:12 -0500

    fixed hydration in ProductCardSkeleton

--- src/app/(customerFacing)/page.tsx
+++ src/app/(customerFacing)/page.tsx
@@ -1,4 +1,4 @@
-import { ProductCard } from "@/components/ProductCard"
+import { ProductCard, ProductCardSkeleton } from "@/components/ProductCard"
 import { Button } from "@/components/ui/button"
 import db from "@/db/db"
 import { Product } from "@prisma/client"
@@ -61,7 +61,7 @@ async function ProductGridSection({ productsFetcher, title}: ProductGridSectionP
 
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
                 {(await productsFetcher()).map(product => (
-                    <ProductCard key={product.id} {...product} />
+                    <ProductCardSkeleton key={product.id} {...product} />
                 ))}
             </div>
         </div>
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -46,24 +46,24 @@ export function ProductCard({ id, name, priceInCents, description, imagePath }: 
 
 export function ProductCardSkeleton() {
     return (
-        <Card className='flex overflow-hidden flex-col'>
-            <div className='relative w-full h-auto aspect-video' />
-            <CardHeader>
-                <CardTitle>
-                    <div className='w-3/4 h-6 rounded-full bg-gray-300' />
-                </CardTitle>
-                <CardDescription>
-                    <div className='w-1/2 h-4 rounded-full bg-gray-300' />
-                </CardDescription>
-            </CardHeader>
-            <CardContent className='flex-grow'>
-                <div className='w-full h-4 rounded-full bg-gray-300' />
-                <div className='w-full h-4 rounded-full bg-gray-300' />
-                <div className='w-full h-4 rounded-full bg-gray-300' />
-            </CardContent>
-            <CardFooter>
-                <Button disabled size='lg' className='w-full'></Button>
-            </CardFooter>
-        </Card>        
+      <Card className="overflow-hidden flex flex-col animate-pulse">
+        <div className="w-full aspect-video bg-gray-300" />
+        <CardHeader>
+          <CardTitle>
+            <div className="w-3/4 h-6 rounded-full bg-gray-300" />
+          </CardTitle>
+          <CardDescription>
+            <span className="w-1/2 h-4 rounded-full bg-gray-300" />
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-2">
+          <div className="w-full h-4 rounded-full bg-gray-300" />
+          <div className="w-full h-4 rounded-full bg-gray-300" />
+          <div className="w-3/4 h-4 rounded-full bg-gray-300" />
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full" disabled size="lg"></Button>
+        </CardFooter>
+      </Card>
     )
-}
+  }

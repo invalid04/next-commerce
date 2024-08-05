0d367e17f3d8aafaba18a6f14918051664f26009
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-05 05:47:04 -0500

    creating stripe function

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,6 +1,10 @@
 import db from "@/db/db"
 import { notFound } from "next/navigation"
 
+import Stripe from 'stripe'
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+
 export default async function PurchasePage({
     params: { id }
 }: {

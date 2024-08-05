e7bc3303eafc78bbb61f7d6377b8f30aabe7e335
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-05 05:17:32 -0500

    added  more skeletons to products page

--- src/app/(customerFacing)/products/page.tsx
+++ src/app/(customerFacing)/products/page.tsx
@@ -18,6 +18,9 @@ export default function ProductsPage() {
                 <ProductCardSkeleton />
                 <ProductCardSkeleton />
                 <ProductCardSkeleton />
+                <ProductCardSkeleton />
+                <ProductCardSkeleton />
+                <ProductCardSkeleton />
             </>
         }>
             <ProductsSuspense />

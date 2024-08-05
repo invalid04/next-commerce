b6168b2ccf7365e3f4e1a672d737ecd59206e1de
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-05 05:21:58 -0500

    callback type

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -1,6 +1,8 @@
 import { unstable_cache as nextCache } from "next/cache";
 import { cache as reactCache } from "react";
 
+type Callback = (...args: any[]) => Promise<any>
+
 export function cache(cb, keyParts, options) {
 
 }

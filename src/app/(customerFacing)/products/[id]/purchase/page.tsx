bbe5fe089a856d6dd8186e0cee9f6ed7204ec1aa
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-05 05:52:41 -0500

    fixed typescript error in stripe variable

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -3,7 +3,7 @@ import { notFound } from "next/navigation"
 
 import Stripe from 'stripe'
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
 export default async function PurchasePage({
     params: { id }

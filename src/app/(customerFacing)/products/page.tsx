41a15bb3c92a37282264c129cda910046ad423d3
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-05 05:16:46 -0500

    fixed export function error

--- src/app/(customerFacing)/products/page.tsx
+++ src/app/(customerFacing)/products/page.tsx
@@ -10,7 +10,7 @@ function getProducts() {
     })
 }
 
-export function ProductPage() {
+export default function ProductsPage() {
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
         <Suspense fallback={

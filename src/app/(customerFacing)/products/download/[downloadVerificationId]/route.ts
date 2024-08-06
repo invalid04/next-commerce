5dc7ed5b9509446789bc844f63423623723d6200
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-06 00:53:43 -0500

    error checking for expiration date

--- src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
+++ src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
@@ -1,3 +1,20 @@
-export function GET() {
-    return 'hi'
+import db from "@/db/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+    req: NextRequest,
+    {
+        params: { downloadVerificationId },
+    }: {
+        params: { downloadVerificationId: string }
+    }
+) {
+    const data = await db.downloadVerification.findUnique({
+        where: { id: downloadVerificationId, expiresAt: { gt: new Date() } },
+        select: { product: { select: { filePath: true, name: true } } },
+    })
+
+    if (data == null) {
+        return NextResponse.redirect(new URL('/products/download/expired', req.url))
+    }
 }

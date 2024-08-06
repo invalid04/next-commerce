1346636f62bdf6c3bd190fda8e0bdd9f228bd532
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-05 23:51:47 -0500

    displaying product info

--- src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { formatCurrency } from '@/lib/formatters'
 import { Elements, PaymentElement, useElements, useStripe } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
 import Image from 'next/image'
@@ -21,20 +22,33 @@ export function CheckoutForm({
     clientSecret
 }: CheckoutFormProps) {
     return (
-        <>
             <div className='max-w-5xl w-full mx-auto space-y-8'>
-                <div className='aspect-video flex-shrink-0 w-1/3 relative'>
-                    <Image 
-                        src={product.imagePath}
-                        fill 
-                        alt={product.name}
-                    />
+                <div className='flex gap-4 items-center'>
+                    <div className='aspect-video flex-shrink-0 w-1/3 relative'>
+                        <Image 
+                            src={product.imagePath}
+                            fill 
+                            alt={product.name}
+                        />
+                    </div>
+
+                    <div>
+                        <div className='text-lg'>
+                            {formatCurrency(product.priceInCents / 100)}
+                        </div>
+                        <h1 className='text-2xl font-bold'>
+                            {product.name}
+                        </h1>
+                        <div className='line-clamp-3 text-muted-foreground'>
+                            {product.description}
+                        </div>
+                    </div>
                 </div>
+                
                 <Elements options={{ clientSecret }} stripe={stripePromise}>
                     <Form />
                 </Elements>    
             </div>    
-        </>
     )
 }
 

a08bf948efe43cf4c7f77c1390a8a0a443ed3d8a
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-05 23:53:48 -0500

    back tabbed the useless whitespace in the form component

--- src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -22,34 +22,34 @@ export function CheckoutForm({
     clientSecret
 }: CheckoutFormProps) {
     return (
-            <div className='max-w-5xl w-full mx-auto space-y-8'>
-                <div className='flex gap-4 items-center'>
-                    <div className='aspect-video flex-shrink-0 w-1/3 relative'>
-                        <Image 
-                            src={product.imagePath}
-                            fill 
-                            alt={product.name}
-                            className='object-cover'
-                        />
-                    </div>
+        <div className='max-w-5xl w-full mx-auto space-y-8'>
+            <div className='flex gap-4 items-center'>
+                <div className='aspect-video flex-shrink-0 w-1/3 relative'>
+                    <Image 
+                        src={product.imagePath}
+                        fill 
+                        alt={product.name}
+                        className='object-cover'
+                    />
+                </div>
 
-                    <div>
-                        <div className='text-lg'>
-                            {formatCurrency(product.priceInCents / 100)}
-                        </div>
-                        <h1 className='text-2xl font-bold'>
-                            {product.name}
-                        </h1>
-                        <div className='line-clamp-3 text-muted-foreground'>
-                            {product.description}
-                        </div>
+                <div>
+                    <div className='text-lg'>
+                        {formatCurrency(product.priceInCents / 100)}
+                    </div>
+                    <h1 className='text-2xl font-bold'>
+                        {product.name}
+                    </h1>
+                    <div className='line-clamp-3 text-muted-foreground'>
+                        {product.description}
                     </div>
                 </div>
-                
-                <Elements options={{ clientSecret }} stripe={stripePromise}>
-                    <Form />
-                </Elements>    
-            </div>    
+            </div>
+            
+            <Elements options={{ clientSecret }} stripe={stripePromise}>
+                <Form />
+            </Elements>    
+        </div>    
     )
 }
 

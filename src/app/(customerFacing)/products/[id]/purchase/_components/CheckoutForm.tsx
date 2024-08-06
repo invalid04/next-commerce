65b044f61785802fc05dbf0d1713a83106db4acd
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-05 23:58:54 -0500

    checkout form styling

--- src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { formatCurrency } from '@/lib/formatters'
 import { Elements, PaymentElement, useElements, useStripe } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
@@ -53,9 +55,38 @@ export function CheckoutForm({
     )
 }
 
-function Form() {
+function Form({ 
+    priceInCents 
+}: {
+    priceInCents: number
+}) {
     const stripe = useStripe()
     const elements = useElements()
 
-    return <PaymentElement />
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>
+                    Checkout
+                </CardTitle>
+                <CardDescription className='text-destructive'>
+                    Error
+                </CardDescription>
+            </CardHeader>
+
+            <CardContent>
+                <PaymentElement />
+            </CardContent>
+
+            <CardFooter>
+                <Button
+                    className='w-full'
+                    size='lg'
+                    disabled={stripe == null || elements == null}
+                >
+                    Purchase - {formatCurrency(priceInCents / 100)}
+                </Button>
+            </CardFooter>
+        </Card>
+    )
 }

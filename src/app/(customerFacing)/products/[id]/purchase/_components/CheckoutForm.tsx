1271c38f2d4be01ef7d3c11851af65111a1d1a5c
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-05 23:40:47 -0500

    Form component

--- src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -1,4 +1,4 @@
-import { Elements } from '@stripe/react-stripe-js'
+import { Elements, PaymentElement, useElements, useStripe } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
 
 type CheckoutFormProps = {
@@ -6,15 +6,22 @@ type CheckoutFormProps = {
     clientSecret: string
 }
 
-const stripe = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string)
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string)
 
 export function CheckoutForm({ 
     product, 
     clientSecret
 }: CheckoutFormProps) {
     return (
-        <Elements options={{ clientSecret }} stripe={stripe}>
-
+        <Elements options={{ clientSecret }} stripe={stripePromise}>
+            <Form />
         </Elements>
     )
+}
+
+function Form() {
+    const stripe = useStripe()
+    const elements = useElements()
+
+    return <PaymentElement />
 }

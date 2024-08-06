c621b12ad21909880a26aea0f145eda680f1df32
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-06 00:58:25 -0500

    Link expired page

---
+++ src/app/(customerFacing)/products/download/expired/page.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function Expired() {
+    return ( 
+        <>
+            <h1 className='text-4xl mb-4'>
+                Download link has expired 
+            </h1>
+            <Button
+                asChild 
+                size='lg'
+            >
+                <Link href='/orders'>
+                    Get New Link
+                </Link>
+            </Button>
+        </>
+    )
+}

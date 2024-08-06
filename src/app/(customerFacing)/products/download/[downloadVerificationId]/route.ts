c2b8513fe160225b56c753526395ce9b9fcf5162
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-06 00:56:00 -0500

    download link now downloads file

--- src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
+++ src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
@@ -1,3 +1,4 @@
+import fs from 'fs/promises';
 import db from "@/db/db";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -17,4 +18,13 @@ export async function GET(
     if (data == null) {
         return NextResponse.redirect(new URL('/products/download/expired', req.url))
     }
+
+    const { size } = await fs.stat(data.product.filePath)
+    const file = await fs.readFile(data.product.filePath)
+    const extension = data.product.filePath.split('.').pop()
+
+    return new NextResponse(file, { headers: {
+        'Content-Disposition': `attachment; filename='${data.product.name}.${extension}'`,
+        'Content-Length': size.toString(),
+    }})
 }

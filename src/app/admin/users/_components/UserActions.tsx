e801df446c643153def2c271c6703775c2e0fd9d
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-07 04:01:37 -0500

    design of delete user component finished

--- src/app/admin/users/_components/UserActions.tsx
+++ src/app/admin/users/_components/UserActions.tsx
@@ -1,5 +1,17 @@
+import { DropdownMenuItem } from "@/components/ui/dropdown-menu"
+import { useRouter } from "next/navigation"
+import { useTransition } from "react"
+
 export function DeleteDropDownItem() {
+    const [isPending, startTransition] = useTransition()
+    const router = useRouter()
+
     return (
-        <h1>hi</h1>
+        <DropdownMenuItem
+            disabled={isPending}
+            
+        >
+            Delete
+        </DropdownMenuItem>
     )
 }

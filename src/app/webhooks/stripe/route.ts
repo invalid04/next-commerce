f4b28ad5355a979603d785bd7d83e715d6838761
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-07 03:29:06 -0500

    created download verification link

--- src/app/webhooks/stripe/route.ts
+++ src/app/webhooks/stripe/route.ts
@@ -36,5 +36,12 @@ export async function POST(req: NextRequest) {
             update: userFields,
             select: { orders: { orderBy: { createdAt: 'desc' }, take: 1 }}
         })
+
+        const downloadVerification = await db.downloadVerification.create({
+            data: {
+                productId,
+                expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24),
+            }
+        })
     }
 }

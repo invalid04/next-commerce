8cb944306286d8b98c2c6e91700b7625ee6ae095
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-07 03:46:02 -0500

    getUsers function

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,3 +1,16 @@
+import db from "@/db/db"
+
+function getUsers() {
+    return db.user.findMany({
+        select: {
+            id: true,
+            email: true,
+            orders: { select: { pricePaidInCents: true } },
+        },
+        orderBy: { createdAt: 'desc' },
+    })
+}
+
 export default function UsersPage() {
     return (
         <h1>hi</h1>

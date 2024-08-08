aa6b749b621ea38e3fac7a61691f531ed46e289b
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-08 05:45:32 -0500

    showing OrderInformation in PurchaseReceipt component

--- src/email/PurchaseReceipt.tsx
+++ src/email/PurchaseReceipt.tsx
@@ -1,4 +1,5 @@
 import { Body, Container, Head, Heading, Html, Preview, Tailwind } from '@react-email/components'
+import OrderInformation from './components/OrderInformation'
 
 export default function PurchaseReceiptEmail({
     product
@@ -14,6 +15,7 @@ export default function PurchaseReceiptEmail({
                 <Body className='font-sans bg-white'>
                     <Container className='max-w-xl'>
                         <Heading>Purchase Receipt</Heading>
+                        <OrderInformation />
                     </Container>
                 </Body>
             </Tailwind>

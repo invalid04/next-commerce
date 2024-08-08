92aee7a9146281c0805810a29c00abb063df5b02
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-08 05:13:46 -0500

    getOrders function

--- src/app/admin/orders/page.tsx
+++ src/app/admin/orders/page.tsx
@@ -1,3 +1,17 @@
+import db from "@/db/db"
+
+function getOrders() {
+    return db.order.findMany({
+        select: {
+            id: true,
+            pricePaidInCents: true,
+            product: { select: { name: true } },
+            user: { select: { email: true } },
+        },
+        orderBy: { createdAt: 'desc' },
+    })
+}
+
 export default function OrdersPage() {
     return (
         <h1>Order</h1>

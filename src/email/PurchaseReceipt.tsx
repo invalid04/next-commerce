5df78b3f5364ba2236d5080ec59137b09cfad37b
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-08 05:43:00 -0500

    purchase receipt preview and layout

--- src/email/PurchaseReceipt.tsx
+++ src/email/PurchaseReceipt.tsx
@@ -1,9 +1,20 @@
-import { Html } from '@react-email/components'
+import { Body, Head, Html, Preview, Tailwind } from '@react-email/components'
 
-export default function PurchaseReceiptEmail() {
+export default function PurchaseReceiptEmail({
+    product
+}) {
     return (
         <Html>
+            <Preview>
+                Download {product.name} and view receipt
+            </Preview>
+            <Tailwind>
+                <Head />
+                
+                <Body>
 
+                </Body>
+            </Tailwind>
         </Html>
     )
 }

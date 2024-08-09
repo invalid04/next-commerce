eac778623fc11630cf3cc4aee75f8b25da390e00
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-09 03:32:01 -0500

    orderInformationProps type

--- src/email/components/OrderInformation.tsx
+++ src/email/components/OrderInformation.tsx
@@ -1,4 +1,14 @@
-export default function OrderInformation() {
+type OrderInformationProps = {
+    order: {}
+    product: {}
+    downloadVerificationId: string
+}
+
+export default function OrderInformation({
+    order,
+    product,
+    downloadVerificationId
+}: OrderInformationProps) {
     return (
         <h1>hi</h1>
     )

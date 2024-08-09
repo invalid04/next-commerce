5c35a7a162f7ea6fc56a1aaa3e8973e38fe1720b
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-09 04:10:20 -0500

    Card design done

--- src/app/(customerFacing)/orders/page.tsx
+++ src/app/(customerFacing)/orders/page.tsx
@@ -1,5 +1,36 @@
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export default function MyOrdersPage() {
     return (
-        <h1>hi</h1>
+        <form>
+            <Card>
+                <CardHeader>
+                    <CardTitle>My Orders</CardTitle>
+                    <CardDescription>
+                        Enter your email to receive your order history and download links
+                    </CardDescription>
+                </CardHeader>
+
+                <CardContent>
+                    <div className='space-y-2'>
+                        <Label 
+                            htmlFor='email'
+                        >
+                            Email
+                        </Label>
+                        <Input 
+                            type='email'
+                            required 
+                            name='email'
+                            id='email'
+                        />
+                    </div>
+                </CardContent>
+
+                <CardFooter></CardFooter>
+            </Card>
+        </form>
     )
 }

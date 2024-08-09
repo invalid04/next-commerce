c0b2b9d6df42177720d6293c7663c9c94ce5ee37
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-09 04:45:12 -0500

    Finished email

--- src/email/OrderHistory.tsx
+++ src/email/OrderHistory.tsx
@@ -1,4 +1,15 @@
-import { Body, Container, Head, Heading, Html, Preview, Tailwind } from "@react-email/components"
+import { 
+    Body, 
+    Container, 
+    Head, 
+    Heading, 
+    Hr, 
+    Html, 
+    Preview, 
+    Tailwind 
+} from "@react-email/components"
+import React from 'react'
+import OrderInformation from "./components/OrderInformation"
 
 type OrderHistoryEmailProps = {
     orders: {
@@ -52,6 +63,16 @@ export default function OrderHistoryEmail({
                 <Body className='font-sans bg-white'>
                     <Container className='max-w-xl'>
                         <Heading>Order History</Heading>
+                        {orders.map((order, index) => (
+                            <React.Fragment key={order.id}>
+                                <OrderInformation 
+                                    order={order}
+                                    product={order.product}
+                                    downloadVerificationId={order.downloadVerificationId}
+                                />
+                                {index < orders.length - 1 && <Hr />}
+                            </React.Fragment>
+                        ))}
                     </Container>
                 </Body>
             </Tailwind>

406a0c466c26c46d32a0dddb6b1411de9ea4b6f0
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-09 03:36:40 -0500

    dateFormatter function

--- src/email/components/OrderInformation.tsx
+++ src/email/components/OrderInformation.tsx
@@ -1,8 +1,12 @@
+import { Column, Row, Section, Text } from "@react-email/components"
+
 type OrderInformationProps = {
     order: {}
     product: {}
     downloadVerificationId: string
 }
+
+const dateFormatter = new Intl.DateTimeFormat('en', { dateStyle: 'medium' })
 
 export default function OrderInformation({
     order,
@@ -10,6 +14,39 @@ export default function OrderInformation({
     downloadVerificationId
 }: OrderInformationProps) {
     return (
-        <h1>hi</h1>
+        <Section>
+            <Row>
+                <Column>
+                    <Text 
+                        className='mb-0 text-gray-500 whitespace-nowrap text-nowrap mr-4'
+                    >
+                        Order ID
+                    </Text>
+                    <Text className='mt-0 mr-4'>
+                        {order.id}
+                    </Text>
+                </Column>
+                <Column>
+                    <Text 
+                        className='mb-0 text-gray-500 whitespace-nowrap text-nowrap mr-4'
+                    >
+                        Purchased On
+                    </Text>
+                    <Text className='mt-0 mr-4'>
+                        {dateFormatter.format(order.createdAt)}
+                    </Text>
+                </Column>
+                <Column>
+                    <Text 
+                        className='mb-0 text-gray-500 whitespace-nowrap text-nowrap mr-4'
+                    >
+                        Order ID
+                    </Text>
+                    <Text className='mt-0 mr-4'>
+                        {order.id}
+                    </Text>
+                </Column>
+            </Row>
+        </Section>
     )
 }

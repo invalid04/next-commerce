d5b03562fca4a7fb8992919a63066f3385d30ba0
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-09 04:39:55 -0500

    PreviewProps

--- src/email/OrderHistory.tsx
+++ src/email/OrderHistory.tsx
@@ -12,6 +12,33 @@ type OrderHistoryEmailProps = {
     }[]
 }
 
+OrderHistoryEmail.PreviewProps = {
+    orders: [
+        {
+            id: crypto.randomUUID(),
+            createdAt: new Date(),
+            pricePaidInCents: 10000,
+            downloadVerificationId: crypto.randomUUID(),
+            product: {
+                name: 'Product name',
+                description: 'description',
+                imagePath: '/products/794c2e81-4a8c-439f-b649-7031848188c4-cartoon beaver  0f8c71d1-539d-45a9-b83a-bd22d2567cf5.png'
+            },
+        },
+        {
+            id: crypto.randomUUID(),
+            createdAt: new Date(),
+            pricePaidInCents: 2000,
+            downloadVerificationId: crypto.randomUUID(),
+            product: {
+                name: 'product 2',
+                description: 'description 2',
+                imagePath: '/products/794c2e81-4a8c-439f-b649-7031848188c4-cartoon beaver  0f8c71d1-539d-45a9-b83a-bd22d2567cf5.png'
+            },
+        },
+    ],
+} satisfies OrderHistoryEmailProps
+
 export default function OrderHistoryEmail({
     orders
 }: OrderHistoryEmailProps) {

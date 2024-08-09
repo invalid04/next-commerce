c83038d9fcdf478a389915fe2683821f0d07b071
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-08-09 04:35:14 -0500

    OrderHistoryEmailProps type

--- src/email/OrderHistory.tsx
+++ src/email/OrderHistory.tsx
@@ -1,3 +1,17 @@
+type OrderHistoryEmailProps = {
+    orders: {
+        id: string
+        pricePaidInCents: number 
+        createdAt: Date 
+        downloadVerificationId: string 
+        product: {
+            name: string 
+            imagePath: string 
+            description: string
+        }
+    }[]
+}
+
 export default function OrderHistoryEmail({
     orders
 }: OrderHistoryEmailProps) {
